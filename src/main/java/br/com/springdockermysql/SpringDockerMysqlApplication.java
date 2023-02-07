d986f2ea2a918a666794bfd53764d688900b45c0
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2023-02-07 16:45:54 -0300

    Construindo uma Api Rest com Spring Boot, Mysql, JPA Repostory e fazendo deploy na Docker

---
+++ src/main/java/br/com/springdockermysql/SpringDockerMysqlApplication.java
@@ -0,0 +1,13 @@
+package br.com.springdockermysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springdockermysql/SpringDockerMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springdockermysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
